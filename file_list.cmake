88f641098416fde3722264293beb4a87fbf48654
quandangv <quandangv@gmail.com>
2021-01-20 11:25:10 +0700

    Message if googletest submodule not found or code coverage report fail

--- file_list.cmake
+++ file_list.cmake
@@ -1,9 +1,10 @@
 # paths to various directories
 get_filename_component(GENERATED_HEADERS_DIR ${CMAKE_BINARY_DIR}/generated-headers ABSOLUTE)
-get_filename_component(INCLUDE_DIR ${CMAKE_SOURCE_DIR}/include ABSOLUTE)
+get_filename_component(INCLUDE_DIR         ${CMAKE_SOURCE_DIR}/include ABSOLUTE)
 get_filename_component(PRIVATE_HEADERS_DIR ${CMAKE_SOURCE_DIR}/private-headers ABSOLUTE)
-get_filename_component(SRC_DIR ${CMAKE_SOURCE_DIR}/src ABSOLUTE)
-get_filename_component(TEST_DIR ${CMAKE_SOURCE_DIR}/test ABSOLUTE)
+get_filename_component(SRC_DIR             ${CMAKE_SOURCE_DIR}/src ABSOLUTE)
+get_filename_component(TEST_DIR            ${CMAKE_SOURCE_DIR}/test ABSOLUTE)
+get_filename_component(LIBRARY_DIR         ${CMAKE_SOURCE_DIR}/lib ABSOLUTE)
 set(HEADER_DIRS ${INCLUDE_DIR} ${PRIVATE_HEADERS_DIR} ${GENERATED_HEADERS_DIR})
 
 # configure files {{{
@@ -21,10 +22,14 @@ set(HEADERS
 )
 
 # source files
-set(SOURCES
+set(TSTRING_SOURCES
   ${SRC_DIR}/tstring.cpp
   ${SRC_DIR}/tstring_utils.cpp
+)
+set(TOKEN_ITERATOR_SOURCES
   ${SRC_DIR}/token_iterator.cpp
 )
+set(SOURCES ${TSTRING_SOURCES} ${TOKEN_ITERATOR_SOURCES})
 
+# tests
 set(EXTERNAL_TESTS tstring)
